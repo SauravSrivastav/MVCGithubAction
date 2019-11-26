b8a03cf604529aaf49799b779f069eedbb4e18ab
SrivastavSaurav <sausr@equinor.com>
2019-11-26 22:26:45 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureActionMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
